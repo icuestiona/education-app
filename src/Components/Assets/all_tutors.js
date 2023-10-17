54e7acc59be13239eab2cb66d78bcad01228867f
Soraya <icuestiona@gmail.com>
2023-10-17 15:37:01 +0200

    Images added in Assets

---
+++ src/Components/Assets/all_tutors.js
@@ -0,0 +1,329 @@
+import t1_img from "./tutor1.jpg";
+import t2_img from "./tutor2.jpg";
+import t3_img from "./tutor3.jpg";
+import t4_img from "./tutor4.jpg";
+import t5_img from "./tutor5.jpg";
+import t6_img from "./tutor6.jpg";
+import t7_img from "./tutor7.jpg";
+import t8_img from "./tutor8.jpg";
+import t9_img from "./tutor9.jpg";
+import t10_img from "./tutor10.jpg";
+import t11_img from "./tutor11.jpg";
+import t12_img from "./tutor12.jpg";
+import t13_img from "./tutor13.jpg";
+import t14_img from "./tutor14.jpg";
+import t15_img from "./tutor15.jpg";
+import t16_img from "./tutor16.jpg";
+import t17_img from "./tutor17.jpg";
+import t18_img from "./tutor18.jpg";
+import t19_img from "./tutor19.jpg";
+import t20_img from "./tutor20.jpg";
+import t21_img from "./tutor21.jpg";
+import t22_img from "./tutor22.jpg";
+import t23_img from "./tutor23.jpg";
+import t24_img from "./tutor24.jpg";
+import t25_img from "./tutor25.jpg";
+import t26_img from "./tutor26.jpg";
+import t27_img from "./tutor27.jpg";
+import t28_img from "./tutor28.jpg";
+import t29_img from "./tutor29.jpg";
+import t30_img from "./tutor30.jpg";
+import t31_img from "./tutor31.jpg";
+import t32_img from "./tutor32.jpg";
+import t33_img from "./tutor33.jpg";
+import t34_img from "./tutor34.jpg";
+import t35_img from "./tutor35.jpg";
+import t36_img from "./tutor36.jpg";
+
+let all_tutors = [
+  {
+    id: 1,
+    name: "Oscar Simmons",
+    subject: "Mathematics",
+    availability: "weekday",
+    schedule: "morning",
+    image: t1_img,
+  },
+  {
+    id: 2,
+    name: "Carla Wilson",
+    subject: "English",
+    availability: "weekday",
+    schedule: "afternoon",
+    image: t2_img,
+  },
+  {
+    id: 3,
+    name: "Ian Evans",
+    subject: "Literature",
+    availability: "weekend",
+    schedule: "morning",
+    image: t3_img,
+  },
+  {
+    id: 4,
+    name: "Marla Johnson",
+    subject: "Chemistry ",
+    availability: "weekend",
+    schedule: "afternoon",
+    image: t4_img,
+  },
+  {
+    id: 5,
+    name: "Cora Campbell",
+    subject: "Computer Science",
+    availability: "weekday",
+    schedule: "evening",
+    image: t5_img,
+  },
+  {
+    id: 6,
+    name: "Lena Lewis",
+    subject: "Chemistry",
+    availability: "weekday",
+    schedule: "morning",
+    image: t6_img,
+  },
+  {
+    id: 7,
+    name: "Christina Davis",
+    subject: "Biology",
+    availability: "weekday",
+    schedule: "afternoon",
+    image: t7_img,
+  },
+  {
+    id: 8,
+    name: "Helen Allen",
+    subject: "Literature",
+    availability: "weekday",
+    schedule: "evening",
+    image: t8_img,
+  },
+  {
+    id: 9,
+    name: "Glenda Smith",
+    subject: "Mathematics",
+    availability: "weekend",
+    schedule: "evening",
+    image: t9_img,
+  },
+  {
+    id: 10,
+    name: "Leo Harris",
+    subject: "English",
+    availability: "weekday",
+    schedule: "afternoon",
+    image: t10_img,
+  },
+  {
+    id: 11,
+    name: "Linda Edwards",
+    subject: "Literature",
+    availability: "weekend",
+    schedule: "morning",
+    image: t11_img,
+  },
+  {
+    id: 12,
+    name: "Benjamin Clark",
+    subject: "Computer Science",
+    availability: "weekday",
+    schedule: "morning",
+    image: t12_img,
+  },
+  {
+    id: 13,
+    name: "Emily Cooper",
+    subject: "Biology",
+    availability: "weekday",
+    schedule: "afternoon",
+    image: t13_img,
+  },
+  {
+    id: 14,
+    name: "Alice Hill",
+    subject: "Biology",
+    availability: "weekday",
+    schedule: "evening",
+    image: t14_img,
+  },
+  {
+    id: 15,
+    name: "Amelia Moore",
+    subject: "Chemistry ",
+    availability: "weekend",
+    schedule: "morning",
+    image: t15_img,
+  },
+  {
+    id: 16,
+    name: "Henry Hernandez",
+    subject: "English",
+    availability: "weekday",
+    schedule: "afternoon",
+    image: t16_img,
+  },
+  {
+    id: 17,
+    name: "Mary Bailey",
+    subject: "Computer Science",
+    availability: "weekday",
+    schedule: "evening",
+    image: t17_img,
+  },
+  {
+    id: 18,
+    name: "Evelyn Collins",
+    subject: "Spanish",
+    availability: "weekday",
+    schedule: "afternoon",
+    image: t18_img,
+  },
+  {
+    id: 19,
+    name: "Joseph Fisher",
+    subject: "Literature",
+    availability: "weekend",
+    schedule: "morning",
+    image: t19_img,
+  },
+  {
+    id: 20,
+    name: "David Adams",
+    subject: "Biology",
+    availability: "weekday",
+    schedule: "afternoon",
+    image: t20_img,
+  },
+  {
+    id: 21,
+    name: "James Walker",
+    subject: "Computer Science",
+    availability: "weekend",
+    schedule: "evening",
+    image: t21_img,
+  },
+  {
+    id: 22,
+    name: "Janeth Martin",
+    subject: "Chemistry ",
+    availability: "weekday",
+    schedule: "evening",
+    image: t22_img,
+  },
+  {
+    id: 23,
+    name: "Mia Hughes",
+    subject: "Biology",
+    availability: "weekend",
+    schedule: "evening",
+    image: t23_img,
+  },
+  {
+    id: 24,
+    name: "Ethan Davies",
+    subject: "Spanish",
+    availability: "weekend",
+    schedule: "morning",
+    image: t21_img,
+  },
+  {
+    id: 25,
+    name: "Anna Moore",
+    subject: "Mathematics",
+    availability: "weekday",
+    schedule: "evening",
+    image: t25_img,
+  },
+  {
+    id: 26,
+    name: "Grace Robinson",
+    subject: "English",
+    availability: "weekend",
+    schedule: "afternoon",
+    image: t26_img,
+  },
+  {
+    id: 27,
+    name: "Andre Carter",
+    subject: "Spanish",
+    availability: "weekday",
+    schedule: "afternoon",
+    image: t27_img,
+  },
+  {
+    id: 28,
+    name: "Arthur Lee",
+    subject: "Biology",
+    availability: "weekday",
+    schedule: "evening",
+    image: t28_img,
+  },
+  {
+    id: 29,
+    name: "Sarah Martinez",
+    subject: "Chemistry",
+    availability: "weekend",
+    schedule: "morning",
+    image: t29_img,
+  },
+  {
+    id: 30,
+    name: "Ella Nelson",
+    subject: "Computer Science",
+    availability: "weekend",
+    schedule: "morning",
+    image: t30_img,
+  },
+  {
+    id: 31,
+    name: "Noah Wright",
+    subject: "Biology",
+    availability: "weekday",
+    schedule: "evening",
+    image: t31_img,
+  },
+  {
+    id: 32,
+    name: "Thomas Walker",
+    subject: "Mathematics",
+    availability: "weekday",
+    schedule: "morning",
+    image: t32_img,
+  },
+  {
+    id: 33,
+    name: "Ruth Adams",
+    subject: "English",
+    availability: "weekend",
+    schedule: "evening",
+    image: t33_img,
+  },
+  {
+    id: 34,
+    name: "Daniel Carter",
+    subject: "Literature",
+    availability: "weekday",
+    schedule: "morning",
+    image: t34_img,
+  },
+  {
+    id: 35,
+    name: "Sophia Garcia",
+    subject: "Chemistry ",
+    availability: "weekend",
+    schedule: "evening",
+    image: t35_img,
+  },
+  {
+    id: 36,
+    name: "Emma Davies",
+    subject: "Mathematics",
+    availability: "weekday",
+    schedule: "afternoon",
+    image: t36_img,
+  },
+];
+
+export default all_tutors;
