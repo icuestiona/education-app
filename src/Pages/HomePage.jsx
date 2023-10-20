ff99c563d37e838b005f4a14f560890e82315842
Soraya <icuestiona@gmail.com>
2023-10-20 12:59:41 +0200

    HomePage.jsx modified

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
 import Navbar from "../Components/Navbar/Navbar";
-import { TutorContext } from "../Context/TutorContex";
+import Hero from "../Components/Hero/Hero";
+import Popular from "../Components/Popular/Popular";
+import Offers from "../Components/Offers/Offers";
 
 const HomePage = () => {
-  const [all_tutors] = useState(TutorContext);
-
   return (
     <div>
       <Navbar />
+      <Hero />
+      <Popular />
+      <Offers />
     </div>
   );
 };
