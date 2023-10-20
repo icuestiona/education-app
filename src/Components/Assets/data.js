44dc8e1a7f242fd50a18fa5b95cc3290dbe43dc0
Soraya <icuestiona@gmail.com>
2023-10-20 12:39:55 +0200

    data,js created

---
+++ src/Components/Assets/data.js
@@ -0,0 +1,41 @@
+import t1_img from "./tutor1.jpg";
+import t2_img from "./tutor2.jpg";
+import t3_img from "./tutor3.jpg";
+import t4_img from "./tutor4.jpg";
+
+let data_tutors = [
+  {
+    id: 1,
+    name: "Oscar Simmons",
+    subject: "Mathematics",
+    availability: "weekday",
+    schedule: "morning",
+    image: t1_img,
+  },
+  {
+    id: 2,
+    name: "Carla Wilson",
+    subject: "English",
+    availability: "weekday",
+    schedule: "afternoon",
+    image: t2_img,
+  },
+  {
+    id: 3,
+    name: "Ian Evans",
+    subject: "Literature",
+    availability: "weekend",
+    schedule: "morning",
+    image: t3_img,
+  },
+  {
+    id: 4,
+    name: "Marla Johnson",
+    subject: "Chemistry ",
+    availability: "weekend",
+    schedule: "afternoon",
+    image: t4_img,
+  },
+];
+
+export default data_tutors;
