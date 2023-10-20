60a20585c5b1f6a787ea2fbdf714f8afa424c64d
Soraya <icuestiona@gmail.com>
2023-10-20 12:53:49 +0200

    TutorContex.jsx created and modified

--- src/Context/TutorContext.jsx
+++ src/Context/TutorContext.jsx
@@ -3,12 +3,12 @@ import all_tutors from "../Components/Assets/all_tutors";
 
 export const TutorContext = createContext(null);
 
-const TutorContextProvider = (props) => {
+const TutorContextProvider = (tutor) => {
   const contextValue = { all_tutors };
 
   return (
     <TutorContext.Provider value={contextValue}>
-      {props.children}
+      {tutor.children}
     </TutorContext.Provider>
   );
 };
