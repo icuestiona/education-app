a29678c1c11ac410229a219ecbd08386cf12bd15
Soraya <icuestiona@gmail.com>
2023-10-20 13:00:10 +0200

    App.js & App.css modified

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React from "react";
 import HomePage from "./Pages/HomePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
+import TutorCategory from "./Pages/TutorCategory";
+import Hero from "./Components/Hero/Hero";
+import LoginSignUp from "./Pages/LoginSignUp";
+import Popular from "./Components/Popular/Popular";
+import Offers from "./Components/Offers/Offers";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Navbar />
+      <Hero />
+      <Popular />
+      <Offers />
       <Routes>
         <Route path="/" element={HomePage} />
-        {/* Add more routes for different pages */}
+        <Route path="/tutor" element={TutorCategory} />
+        <Route path="/subject" element={TutorCategory} />
+        <Route path="/availability" element={TutorCategory} />
+        <Route path="/login" element={LoginSignUp} />
       </Routes>
     </BrowserRouter>
   );
