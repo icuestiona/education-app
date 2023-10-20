7f5177ec2fd2d2dc31999850e839fdcb0d96d0ce
Soraya <icuestiona@gmail.com>
2023-10-20 12:46:52 +0200

    Create month_rank.js

---
+++ src/Components/Assets/month_rank.js
@@ -0,0 +1,77 @@
+import t15_img from "./tutor15.jpg";
+import t5_img from "./tutor5.jpg";
+import t24_img from "./tutor24.jpg";
+import t34_img from "./tutor34.jpg";
+import t19_img from "./tutor19.jpg";
+import t8_img from "./tutor8.jpg";
+import t11_img from "./tutor11.jpg";
+import t29_img from "./tutor29.jpg";
+
+let monthly_top_ranked = [
+  {
+    id: 15,
+    name: "Amelia Moore",
+    subject: "Chemistry ",
+    availability: "weekend",
+    schedule: "morning",
+    image: t15_img,
+  },
+  {
+    id: 5,
+    name: "Cora Campbell",
+    subject: "Computer Science",
+    availability: "weekday",
+    schedule: "evening",
+    image: t5_img,
+  },
+  {
+    id: 24,
+    name: "Ethan Davies",
+    subject: "Spanish",
+    availability: "weekend",
+    schedule: "morning",
+    image: t24_img,
+  },
+  {
+    id: 34,
+    name: "Daniel Carter",
+    subject: "Literature",
+    availability: "weekday",
+    schedule: "morning",
+    image: t34_img,
+  },
+  {
+    id: 19,
+    name: "Joseph Fisher",
+    subject: "Literature",
+    availability: "weekend",
+    schedule: "morning",
+    image: t19_img,
+  },
+  {
+    id: 8,
+    name: "Helen Allen",
+    subject: "Literature",
+    availability: "weekday",
+    schedule: "evening",
+    image: t8_img,
+  },
+  {
+    id: 11,
+    name: "Linda Edwards",
+    subject: "Literature",
+    availability: "weekend",
+    schedule: "morning",
+    image: t11_img,
+  },
+  {
+    id: 29,
+    name: "Sarah Martinez",
+    subject: "Chemistry",
+    availability: "weekend",
+    schedule: "morning",
+    image: t29_img,
+  },
+];
+
+export default monthly_top_ranked;
