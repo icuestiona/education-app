450077f0ab6f9356d751f3f86b3d6f7bcb80ace4
Soraya <icuestiona@gmail.com>
2023-10-21 21:23:35 +0200

    LoginSignUp created and styled

--- src/Pages/LoginSignUp.jsx
+++ src/Pages/LoginSignUp.jsx
@@ -1,7 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import "./CSS/LoginSignUp.css";
+import user_icon from "../Components/Assets/person.png";
+import email_icon from "../Components/Assets/email-50.png";
+import password_icon from "../Components/Assets/password-50.png";
 
-const LoginSignUp = () => {
-  return <div>LoginSignUp</div>;
+export const LoginSignUp = () => {
+  const [action, setAction] = useState("Login");
+
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="text">{action}</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        {action === "Login" ? (
+          <div></div>
+        ) : (
+          <div className="input">
+            <img src={user_icon} alt="" />
+            <input type="text" placeholder="Name" />
+          </div>
+        )}
+
+        <div className="input">
+          <img src={email_icon} alt="" />
+          <input type="email" placeholder="Email Id" />
+        </div>
+        <div className="input">
+          <img src={password_icon} alt="" />
+          <input type="password" placeholder="Password" />
+        </div>
+      </div>
+      {action === "Sign Up" ? (
+        <div></div>
+      ) : (
+        <div className="forgot-password">
+          Lost Password? <span>Click Here!</span>{" "}
+        </div>
+      )}
+
+      <div className="submit-container">
+        <div
+          className={action === "Login" ? "submit gray" : "submit"}
+          onClick={() => {
+            setAction("Sign Up");
+          }}
+        >
+          Sign Up
+        </div>
+        <div
+          className={action === "Sign Up" ? "submit gray" : "submit"}
+          onClick={() => {
+            setAction("Login");
+          }}
+        >
+          Login
+        </div>
+      </div>
+      <div className="logingsignup-agree">
+        <input type="checkbox" name="" id="" />
+        <p>By continuing, I agree to the terms of use & privacy policy </p>
+      </div>
+    </div>
+  );
 };
-
 export default LoginSignUp;
