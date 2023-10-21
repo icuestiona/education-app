1887a249c06db4185362e8edc281b2bb25caaf41
Soraya <icuestiona@gmail.com>
2023-10-21 21:26:03 +0200

    App.js modified adding Route path of different elements

--- src/App.js
+++ src/App.js
@@ -3,25 +3,35 @@ import HomePage from "./Pages/HomePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
 import TutorCategory from "./Pages/TutorCategory";
-import Hero from "./Components/Hero/Hero";
 import LoginSignUp from "./Pages/LoginSignUp";
-import Popular from "./Components/Popular/Popular";
-import Offers from "./Components/Offers/Offers";
+import About from "./Pages/About";
+import Footer from "./Components/Footer/Footer";
+import JoinUs from "./Pages/JoinUs";
+import Donate from "./Pages/Donate";
+import Contact from "./Pages/Contact";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Navbar />
-      <Hero />
-      <Popular />
-      <Offers />
       <Routes>
-        <Route path="/" element={HomePage} />
-        <Route path="/tutor" element={TutorCategory} />
-        <Route path="/subject" element={TutorCategory} />
-        <Route path="/availability" element={TutorCategory} />
-        <Route path="/login" element={LoginSignUp} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/tutors" element={<TutorCategory category="tutor" />} />
+        <Route
+          path="/subjects"
+          element={<TutorCategory category="subject" />}
+        />
+        <Route
+          path="/availability"
+          element={<TutorCategory category="availability" />}
+        />
+        <Route path="/join" element={<JoinUs />} />
+        <Route path="/donate" element={<Donate />} />
+        <Route path="/login" element={<LoginSignUp />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
